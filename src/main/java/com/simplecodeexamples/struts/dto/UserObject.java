e4d4a6d17ce8dc37425974056aec4f04d1c40720
biswarup.banerjee <biswarupban@gmail.com>
2017-03-10 15:55:28 +0530

    changes

--- src/main/java/com/simplecodeexamples/struts/dto/UserObject.java
+++ src/main/java/com/simplecodeexamples/struts/dto/UserObject.java
@@ -1,33 +1,57 @@
 package com.simplecodeexamples.struts.dto;
- 
+
+import java.util.Date;
+
 public class UserObject {
-  
- private String firstName;
- private String lastName;
- private String userName;
- 
- public String getFirstName() {
-  return firstName;
- }
- 
- public void setFirstName(String firstName) {
-  this.firstName = firstName;
- }
- 
- public String getLastName() {
-  return lastName;
- }
- 
- public void setLastName(String lastName) {
-  this.lastName = lastName;
- }
- 
- public String getUserName() {
-  return userName;
- }
- 
- public void setUserName(String userName) {
-  this.userName = userName;
- }
- 
+	
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private Date dob;
+	private String phone;
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+
 }
--- src/main/java/com/simplecodeexamples/struts/form/UserForm.java
+++ src/main/java/com/simplecodeexamples/struts/form/UserForm.java
@@ -1,48 +1,85 @@
 package com.simplecodeexamples.struts.form;
+
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionMessage;
- 
-public class UserForm extends ActionForm {
-  
- private static final long serialVersionUID = 1L;
-  
- private String userName;
- private String firstName;
- private String lastName;
- 
- public String getUserName() {
-  return userName;
- }
- 
- public void setUserName(String userName) {
-  this.userName = userName;
- }
- 
- public String getFirstName() {
-  return firstName;
- }
- 
- public void setFirstName(String firstName) {
-  this.firstName = firstName;
- }
- 
- public String getLastName() {
-  return lastName;
- }
- 
- public void setLastName(String lastName) {
-  this.lastName = lastName;
- }
 
-  
- @Override
- public void reset(ActionMapping mapping, HttpServletRequest request) {
-  firstName=null;
-  lastName=null;
-  userName=null;
- }
- 
+public class UserForm extends ActionForm {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String userName;
+	private String firstName;
+	private String lastName;
+	private String dob;
+	private String phone;
+	
+
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	@Override
+	public ActionErrors validate(ActionMapping mapping,
+			HttpServletRequest request) {
+		ActionErrors errors = new ActionErrors();
+		if (firstName.equals("")) {
+			errors.add("firstName",new ActionError("first.name.blank"));
+		}
+		else if (lastName.equals("")) {
+			errors.add("lastName",new ActionError("last.name.blank"));
+		}
+		else if (userName.equals("")) {
+			errors.add("userName",new ActionError("user.name.blank"));
+		}
+		return errors;
+	}
+	
+	@Override
+	public void reset(ActionMapping mapping, HttpServletRequest request) {
+		firstName=null;
+		lastName=null;
+		userName=null;
+	}
+
 }
